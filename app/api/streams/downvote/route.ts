236ee8a1c2e038756dd07a356b6eecb02fb7682a
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-30 23:18:06 +0530

    src: added routes for downvotes and upvotes

---
+++ app/api/streams/downvote/route.ts
@@ -0,0 +1,50 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const UpvoteSchema = z.object({
+  streamId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  //TODO: can get RID of the DB Call here
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? "",
+    },
+  });
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: " UnAuthenticated",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  try {
+    const data = await UpvoteSchema.parse(await req.json());
+    await prismaClient.upvote.delete({
+      where: {
+        userId_streamId: {
+          userId: user.id,
+          streamId: data.streamId,
+        },
+      },
+    });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Error while Upvoting",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+}
