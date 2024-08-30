285e4a1cef339a91359732c2cf3405eed1141866
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-30 13:10:40 +0530

    src: added yt and spotify regex for url validation

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,6 +2,13 @@ import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
+const YTRegex = new RegExp(
+  "^(https?://)?(www.)?(youtube.com|youtu.be)/(watch?v=)?([a-zA-Z0-9_-]{11})"
+);
+const SpotifyRegex = new RegExp(
+  "^(https://open.spotify.com/(track|album|playlist|artist)/[a-zA-Z0-9]+)(?si=[a-zA-Z0-9]+)?$"
+);
+
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
   //regex check for spotify or youtube; TODO!
@@ -13,7 +20,12 @@ export async function POST(req: NextRequest) {
   //using zod to parse the correct data
   try {
     const data = CreateStreamSchema.safeParse(await req.json());
-    prismaClient.stream.create({});
+    const isYt = data.url.includes("youtube");
+
+    prismaClient.stream.create({
+      userId: data.creatorId,
+      url: data.url,
+    });
   } catch (e) {
     return NextResponse.json(
       {
---
+++ backend.md
@@ -0,0 +1,6 @@
+## main backend routes:
+
+1. Users can add streams 
+2. Users upvote/cancel upvotes on streams 
+3. Lets everyone fetch the current streams (with their upvotes)
+
