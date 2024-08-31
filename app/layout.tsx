3648e7f90bab2fa616ac83af2d1ed70a60f1b5f5
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-31 11:58:35 +0530

    ui: no scrollbar on global level

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="no-scrollbar">
       <Providers>
         <body className={inter.className}>{children}</body>
       </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
         </div>
       </section>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-gray-700">
-        <p className="text-xs text-gray-400">© 2023 MusicStreamChoice. All rights reserved.</p>
+        <p className="text-xs text-gray-400">© 2024 jukeboxmonkey. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs text-gray-400 hover:text-purple-400 transition-colors" href="#">
             Terms of Service
