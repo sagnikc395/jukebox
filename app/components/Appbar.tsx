b87d8b1228feabbe5132710bb20be6b9249d90a1
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-31 12:07:40 +0530

    ui: button fixes and main logo created

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -2,7 +2,6 @@
 import { signIn, useSession } from "next-auth/react"
 import { signOut } from "next-auth/react";
 
-import { Music } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export function Appbar() {
@@ -10,11 +9,11 @@ export function Appbar() {
     //check if user already loggedin
     const session = useSession();
     return <div>
-        <div className="flex justify-between">
-            <div>
+        <div className="flex justify-between px-20 pt-6">
+            <div className="text-xl font-bold flex flex-col justify-center text-white">
                 jukebox
             </div>
-            <div>
+            <div className="">
                 {session.data?.user && <Button className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signOut()}>Logout</Button>}
                 {!session.data?.user && <Button className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signIn()}>Signin</Button>}
             </div>
